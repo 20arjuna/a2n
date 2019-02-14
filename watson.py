2b1179b74f1a385631824ae7833812d9f1ae193c
20SahilJ <20sahilj@students.harker.org>
2019-02-13 18:19:04 -0800

    got all of the keywords into a list

--- watson.py
+++ watson.py
@@ -4,6 +4,7 @@ from watson_developer_cloud.natural_language_understanding_v1 import Features, K
 
 file = open("input.txt", "r")
 outputfile = open("output.json", "w")
+keywords = []
 
 natural_language_understanding = NaturalLanguageUnderstandingV1(
     version='2018-11-16',
@@ -19,3 +20,21 @@ print(json.dumps(response, indent=2))
 file.close()
 outputfile.write(json.dumps(response, indent=2))
 outputfile.close()
+with open("output.json", "r") as read_file:
+    data = json.load(read_file)
+keywordslist = data['keywords']
+for x in keywordslist:
+    keywords.append(x['text'])
+for i in keywords:
+    print(i)
+#keyworddata1 = keywordslist[0]
+#print(keyworddata1['text'])
+#newoutputfile = open("output.txt", "r")
+#linelist = newoutputfile.read().splitlines()
+#print("\"text\": ")
+#for x in linelist:
+#    print(x)
+#    if x.startswith("\"text\": "):
+#        keywords.append(x[8:])
+#
+#print(keywords)
