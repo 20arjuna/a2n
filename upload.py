51dce86b84e8d886d8c55086f64e45f5b504ec1b
Arjun Akkiraju <20arjuna@students.harker.org>
2019-02-27 16:22:11 -0800

    print notes

--- upload.py
+++ upload.py
@@ -296,7 +296,7 @@ def upload_file():
     #image.show()
     image.save('cloud.png', 'PNG')
 
-    return send_file('cloud.png')
+    return send_file('notes.txt')
 
 if __name__ == "__main__":
     app.run()
