c03bf37f266b05fcc83bb3d86664d3be99a0e0e8
Arjun Akkiraju <20arjuna@students.harker.org>
2019-03-04 15:43:10 -0800

    returning audio

--- upload.py
+++ upload.py
@@ -267,7 +267,7 @@ def upload_file():
    print('sox is a go!')
 
    # extra argument: result_ttl=5000
-   result = q1.enqueue_call(func=utils.upload_to_google, args=(), timeout='1h')
+   '''result = q1.enqueue_call(func=utils.upload_to_google, args=(), timeout='1h')
    print(' arjun is suhtupid and uploadtogoogle works')
    result = q1.enqueue_call(func=utils.speech_to_text, args=(), timeout='1h')
    print(' arjun is sutupider and speechtotext works')
@@ -278,7 +278,8 @@ def upload_file():
 
    while (result.is_finished != True):
        time.sleep(1)
-   return render_template('fileDownload.html')
+   return render_template('fileDownload.html')'''
+   return send_file('flacified.flac')
 
 if __name__ == "__main__":
     app.run()
