7d784f8f68e200766ac50e8e518767235b778f1b
Arjun Akkiraju <20arjuna@students.harker.org>
2019-03-11 18:00:44 -0700

    my code is better than sahils

--- emailtest.py
+++ emailtest.py
@@ -1,9 +1,8 @@
 import smtplib
-from os.path import basename
-from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.utils import COMMASPACE, formatdate
+from email.mime.base import MIMEBase
+from email import encoders
 
 
 def send_mail(send_from, send_to, subject, text, files=None,
@@ -33,9 +32,66 @@ def send_mail(send_from, send_to, subject, text, files=None,
 
 
     smtp = smtplib.SMTP(server)
-    print('yhhyuy')
+    print('weioewfjoifoijfiojefwioewf')
     smtp.sendmail(send_from, send_to, msg.as_string())
     print('email sent')
     smtp.close()
+def send_mail_arjun(from_address, to_address, file_name, file_path):
+    fromaddr = from_address
+    toaddr = to_address
+
+    # instance of MIMEMultipart
+    msg = MIMEMultipart()
+
+    # storing the senders email address
+    msg['From'] = fromaddr
+
+    # storing the receivers email address
+    msg['To'] = toaddr
+
+    # storing the subject
+    msg['Subject'] = "Your Outline and Wordcloud, courtesy of A2N"
+
+    # string to store the body of the mail
+    body = "Enjoy."
+
+    # attach the body with the msg instance
+    msg.attach(MIMEText(body, 'plain'))
+
+    # open the file to be sent
+    filename = file_name
+    attachment = open(file_path, "rb")
+
+    # instance of MIMEBase and named as p
+    p = MIMEBase('application', 'octet-stream')
+
+    # To change the payload into encoded form
+    p.set_payload((attachment).read())
+
+    # encode into base64
+    encoders.encode_base64(p)
+
+    p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+    # attach the instance 'p' to instance 'msg'
+    msg.attach(p)
+
+    # creates SMTP session
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+
+    # start TLS for security
+    s.starttls()
+
+    # Authentication
+    s.login(fromaddr, "arjunsahil123")
+
+    # Converts the Multipart msg into a string
+    text = msg.as_string()
+
+    # sending the mail
+    s.sendmail(fromaddr, toaddr, text)
+
+    # terminating the session
+    s.quit()
 if __name__ == '__main__':
-    send_mail('20sahilj@students.harker.org', 'a2npelican@gmail.com', 'test', 'this is a test email', None, "127.0.0.1:5000")
+    send_mail_arjun('a2npelican@gmail.com', '20sahilj@students.harker.org', 'requirements.txt', 'requirements.txt')
