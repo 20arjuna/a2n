1e5e601d5df1ed27c0731df0c6edf838fef8cdcb
Arjun Akkiraju <20arjuna@students.harker.org>
2019-03-22 00:03:02 -0700

    this won't work

--- upload.py
+++ upload.py
@@ -262,7 +262,7 @@ def hello():
 def upload_file():
     email = request.form['email']
     utils.send_email(email)
-    #return render_template()
+    return render_template('beta.html')
 #     q = Queue(connection=conn)
 #     print('flacifying LOL')
 #     rawFile = request.files['gcloudfile']
